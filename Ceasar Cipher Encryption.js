c003d7564457b0e90dadf01253373de27544fd2f
CSteigueber <c.steigueber@gmail.com>
2019-11-13 10:56:48 +0100

    switch done

--- Ceasar Cipher Encryption.js
+++ Ceasar Cipher Encryption.js
@@ -1,12 +1,27 @@
 var str="Howdie partner!";
-var cc=13;
+console.log(str);
+var init_cc=13;
+var cc=init_cc;
 var encrypt="";
 var plain="";
 for (let i=0; i<str.length;i++){
     let v=str.charCodeAt(i);
     switch (true){
-        
+        case (v==32): cc++; if (cc==26){cc=0};                     break;
+        case (v>=65 && v+cc<=90)||(v>=95 && v+cc<=122): v+=cc;   break;
+        case (v>= 65 && v<=90) || (v>=95 && v<=122): v+=(cc-26); break;
     }
-    console.log(v);
+    encrypt+=String.fromCharCode(v);
+    
 
 }
+console.log(encrypt);
+ for (let i=0; i<encrypt.length; i++){
+    let v=str.charCodeAt(i);
+    switch (true){
+        case (v==32): cc++; if (cc==26){cc=0};                     break;
+        case (v>=65 && v+cc<=90)||(v>=95 && v+cc<=122): v+=cc;   break;
+        case (v>= 65 && v<=90) || (v>=95 && v<=122): v+=(cc-26); break;
+    }
+    encrypt+=String.fromCharCode(v);
+ }
