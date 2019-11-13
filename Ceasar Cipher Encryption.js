3d24e0908e5f9cdeebf1ecf0371017b701b330df
CSteigueber <c.steigueber@gmail.com>
2019-11-13 10:20:11 +0100

    built the switch

---
+++ Ceasar Cipher Encryption.js
@@ -0,0 +1,12 @@
+var str="Howdie partner!";
+var cc=13;
+var encrypt="";
+var plain="";
+for (let i=0; i<str.length;i++){
+    let v=str.charCodeAt(i);
+    switch (true){
+        
+    }
+    console.log(v);
+
+}
